e513386a07ce5dd3148af745e5032da4a66f0e9c
gremalino <katy.smith1999@hotmail.co.uk>
2025-01-25 19:11:52 +0000

    Dialogue Line Class

--- Assets/Scripts/Data/OrderInfo.cs
+++ Assets/Scripts/Data/OrderInfo.cs
@@ -6,10 +6,16 @@ public class OrderInfo
     public CharacterData customer;
     public CocktailData cocktail;
 
+    public DialogueLine[] introText;
+    public DialogueLine[] failText;
+    public DialogueLine[] successText;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
     [TextArea(3, 10)]
-    public string[] introText;
-    [TextArea(3, 10)]
-    public string[] failText;
-    [TextArea(3, 10)]
-    public string[] successText;
+    public string Text;
+
+    public bool IsMe;
 }
